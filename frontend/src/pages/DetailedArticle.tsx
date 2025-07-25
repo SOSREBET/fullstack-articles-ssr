32ed466a95470bf6754a2bc270de2a4b29c03b3f
SOSREBET <kekcij@mail.ru>
2025-07-25 11:56:23 +0300

    DetailedArticle page

--- frontend/src/pages/DetailedArticle.tsx
+++ frontend/src/pages/DetailedArticle.tsx
@@ -1,28 +1,51 @@
 import type { FC } from "react"
 import { useParams } from "react-router-dom"
 import { useGetArticleQuery } from "../services/articleApi"
-import { Alert, AlertTitle, Grid } from "@mui/material"
+import { Alert, AlertTitle, Breadcrumbs, Chip, Grid, Link as MUILink, Typography } from "@mui/material"
+import BackdropLoader from "../components/BackdropLoader"
+import { Link } from "react-router-dom"
+import { routes } from "../components/Router"
 
 
 const DetailedArticle: FC = () => {
-    const {id: articleId} = useParams()
-    const { data: article, isFetching, isError } =  useGetArticleQuery(articleId ? articleId : '')
+    const { id: articleId } = useParams()
+    const { data: article, isFetching, isError } = useGetArticleQuery(articleId ? articleId : '')
 
     return (
-        <Grid container>
+        <Grid container className='container'>
+            <Breadcrumbs
+                aria-label="breadcrumb"
+                separator="›"
+                sx={{
+                    marginBottom: 2,
+                    '& .MuiBreadcrumbs-separator': {
+                        color: 'white',
+                    },
+                }}
+            >
+                <MUILink underline="hover" color="info" component={Link} to={routes.articles.path}>
+                    Articles
+                </MUILink>
+                {article
+                    ? <Typography sx={{ color: 'white' }}>Article #{article && article.id}</Typography>
+                    : <Typography sx={{ color: 'white' }}>Article not found</Typography>
+                }
+            </Breadcrumbs>
+
             {isError &&
-                <Alert variant="filled" severity="error" >
+                <Grid size={12}><Alert variant="filled" severity="error" >
                     <AlertTitle>Error</AlertTitle>
                     Something went wrong.
-                </Alert>  
+                </Alert></Grid>
             }
             {article &&
                 <Grid>
-                    <h2>{article.title}</h2>
-                    <p>{article.text}</p>
-                    <time>{article.created_at}</time>
+                    <Typography fontFamily={'inherit'} color="info"><h2>{article.title}</h2></Typography>
+                    <Typography fontFamily={'inherit'} marginTop={2}><p>{article.text}</p></Typography>
+                    <time><Chip color="info" label={article.created_at} variant="outlined" sx={{ marginTop: 2 }} /></time>
                 </Grid>
             }
+            {isFetching && <BackdropLoader />}
         </Grid>
     )
 }
