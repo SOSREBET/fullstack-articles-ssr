25df779439951d2a77069d59dc60a4572369398b
SOSREBET <kekcij@mail.ru>
2025-07-25 11:59:00 +0300

    Backend improvement

--- backend/articles/article/filters.py
+++ backend/articles/article/filters.py
@@ -1,10 +1,15 @@
 import django_filters
+from django.db.models import Q
 from .models import ArticleModel
 
 class ArticleFilter(django_filters.FilterSet):
-    title = django_filters.CharFilter(lookup_expr='icontains')
-    text = django_filters.CharFilter(lookup_expr='icontains')
+    search = django_filters.CharFilter(field_name='search', method='filter_search')
 
     class Meta:
         model = ArticleModel
-        fields = ['title', 'text']
+        fields = []
+        
+    def filter_search(self, queryset, name, value):
+        return queryset.filter(
+            Q(title__icontains=value) | Q(text__icontains=value)
+        )
